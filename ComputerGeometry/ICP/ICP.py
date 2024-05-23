857089f3f18536eeb3057e662d74b41ed164171c
NAKANISHI\sygha <syghabc@gmail.com>
2024-05-23 20:03:52 +0900

    ICPにPoint2Plane追加

--- ComputerGeometry/ICP/ICP.py
+++ ComputerGeometry/ICP/ICP.py
@@ -56,22 +56,77 @@ def Point2Point(source, target, max_itr):
     return point
 
 
+#####Point-to-Planeによる点群位置合わせ
+def Point2Plane(source, target, target_normal, max_itr):
+    num = len(source) #点群数
+
+    point = copy.deepcopy(source)
+    for _ in range(max_itr):
+        a = np.concatenate((np.cross(point, target_normal), target_normal), axis = 1)
+        A = (a.T)@a
+
+        _b = (np.sum(target_normal*(target - point), axis = 1)).reshape((num, 1))
+        b = np.sum(_b*a, axis = 0)
+
+        u = np.linalg.solve(A, b)
+        theta = np.linalg.norm(u[:3])
+        w = u[:3]/theta
+        W = np.array([
+            [0., -w[2], w[1]],
+            [w[2], 0., -w[0]],
+            [-w[1], w[0], 0.]
+        ])
+        R = np.eye(3) + np.sin(theta)*W + (1.-np.cos(theta))*(W@W)
+        Trans = np.eye(4)
+        Trans[:3,:3] = R
+        Trans[:3,3] = u[3:]
+
+        point = (Trans@(np.concatenate((point, np.ones((num, 1))), axis = 1).T)).T
+        point = point[:,:-1]
+
+    return point
+
+
 if __name__ == "__main__":
-    target = np.random.rand(100, 3) #ターゲット点群
-    target_geo = pv.UnstructuredGrid()
-    target_geo.points = target
-    target_geo.save("target.vtk")
+    ############ Point-to-Pointのサンプルコード
+    # target = np.random.rand(100, 3) #ターゲット点群
+    # target_geo = pv.UnstructuredGrid()
+    # target_geo.points = target
+    # target_geo.save("target.vtk")
+
+    # theta = np.pi/4.
+    # vec = np.ones(3)
+    # vec = vec/np.linalg.norm(vec) #回転軸
+
+    # source = (rot(theta, vec)@target.T).T + np.array([1., 0., 0.])
+    # source_geo = pv.UnstructuredGrid()
+    # source_geo.points = source
+    # source_geo.save("source.vtk")
+
+    # point = Point2Point(source, target, 100)
+    # point_geo = pv.UnstructuredGrid()
+    # point_geo.points = point
+    # point_geo.save("point.vtk")
+
+    ##### Point-to-Planeのサンプルコード
+    target = pv.read("Maureen.stl")
+    target_normals = target.point_normals
+    target_points = target.points
+    # target_geo = pv.UnstructuredGrid()
+    # target_geo.points = target_points
+    # target_geo.save("target.vtk")
 
     theta = np.pi/4.
     vec = np.ones(3)
     vec = vec/np.linalg.norm(vec) #回転軸
 
-    source = (rot(theta, vec)@target.T).T + np.array([1., 0., 0.])
+    source_points = (rot(theta, vec)@target_points.T).T + np.array([20., 0., 0.])
     source_geo = pv.UnstructuredGrid()
-    source_geo.points = source
+    source_geo.points = source_points
     source_geo.save("source.vtk")
 
-    point = Point2Point(source, target, 100)
+
+    point = Point2Plane(source_points, target_points, target_normals, 100)
     point_geo = pv.UnstructuredGrid()
     point_geo.points = point
     point_geo.save("point.vtk")
